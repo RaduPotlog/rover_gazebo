4d788442523824a74ae8e183c827a38ae3b59a2a
radupotlog <potlogradu@gmail.com>
2025-09-26 10:55:19 +0300

    Add publish description flag

--- launch/include/simulate_robot.launch.py
+++ launch/include/simulate_robot.launch.py
@@ -119,6 +119,7 @@ def generate_launch_description():
         launch_arguments={
             "log_level": log_level,
             "namespace": namespace,
+            "publish_robot_state": "False",
             "use_sim": "True",
         }.items(),
     )
